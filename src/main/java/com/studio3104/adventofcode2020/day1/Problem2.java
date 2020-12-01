d6519df6f9831fce2b0b9b6ba9d4b3bf8171a837
studio3104 <studio3104.com@gmail.com>
2020-12-01 16:06:31 +0900

    Answer Problem 2 of Day 1

---
+++ src/main/java/com/studio3104/adventofcode2020/day1/Problem2.java
@@ -0,0 +1,29 @@
+package com.studio3104.adventofcode2020.day1;
+
+import java.util.Arrays;
+
+public class Problem2 extends Problem1 {
+    private int[] getNumsOfThreeSum(int[] nums) {
+        for (int i = 0; i < nums.length; ++i) {
+            int n = nums[i];
+            int sumTo = SUMTO - n;
+            int[] twoSum = getNumsOfTwoSum(nums, i, sumTo);
+
+            if (twoSum[0] != 0 || twoSum[1] != 0) {
+                return new int[]{n, twoSum[0], twoSum[1]};
+            }
+        }
+
+        return new int[]{0, 0, 0};
+    }
+
+    public int getResult(int[] nums) {
+        int[] threeSum = getNumsOfThreeSum(nums);
+        return Arrays.stream(threeSum).reduce(1, (n, m) -> n * m);
+    }
+
+    public static void main(String[] args) {
+        Problem2 problem = new Problem2();
+        System.out.println(problem.getResult(INPUT));
+    }
+}
