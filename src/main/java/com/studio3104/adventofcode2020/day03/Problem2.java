8a4d6e0f996c5c5e829f47d7cb566e60ead63836
studio3104 <studio3104.com@gmail.com>
2020-12-03 15:31:45 +0900

    Update Problem 2 of Day 3

--- src/main/java/com/studio3104/adventofcode2020/day03/Problem2.java
+++ src/main/java/com/studio3104/adventofcode2020/day03/Problem2.java
@@ -2,16 +2,14 @@ package com.studio3104.adventofcode2020.day03;
 
 import com.studio3104.adventofcode2020.utilities.InputLoader;
 
-import java.math.BigInteger;
 import java.util.Arrays;
 
 public class Problem2 {
-    private static BigInteger getResult(String[] map) {
+    private static long getResult(String[] map) {
         int[][] ways = new int[][]{{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}};
         return Arrays.stream(ways)
-                .mapToInt(w -> Problem1.countTrees(map, w[0], w[1]))
-                .mapToObj(BigInteger::valueOf)
-                .reduce(BigInteger.ONE, BigInteger::multiply);
+                .mapToLong(w -> Problem1.countTrees(map, w[0], w[1]))
+                .reduce(1, (n, m) -> n * m);
     }
 
     public static void main(String[] args) {
