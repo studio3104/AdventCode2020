65a052a1fe3b8e40f8339e7d66f3a07ab58ba7ed
studio3104 <studio3104.com@gmail.com>
2020-12-05 18:44:23 +0900

    Answer Problem 2 of Day 5

--- src/main/java/com/studio3104/adventofcode2020/day05/Problem1.java
+++ src/main/java/com/studio3104/adventofcode2020/day05/Problem1.java
@@ -5,40 +5,14 @@ import com.studio3104.adventofcode2020.utilities.InputLoader;
 import java.util.stream.Stream;
 
 public class Problem1 {
-    private static int findPosition(String seat, int tail, int start, int end, char lower, char upper) {
-        int head = 0;
-
-        for (int i = start; i <= end; ++i) {
-            char p = seat.charAt(i);
-            int n = head + (tail - head) / 2;
-            if (p == lower) {
-                tail = n;
-            } else if (p == upper) {
-                head = n + 1;
-            } else {
-                throw new RuntimeException();
-            }
-        }
-
-        return head;
-    }
-
-    static int[] findPosition(String seat) {
-        return new int[]{
-                findPosition(seat, 127, 0, 6, 'F', 'B'),
-                findPosition(seat, 7, 7, 9, 'L', 'R')
-        };
-    }
-
-    private static int getResult(Stream<String> seats) {
+    private static int getResult(Stream<Seat> seats) {
         return seats
-                .map(Problem1::findPosition)
-                .mapToInt(p -> p[0] * 8 + p[1])
+                .mapToInt(Seat::getId)
                 .max()
                 .orElse(0);
     }
 
     public static void main(String[] args) {
-        System.out.println(getResult(InputLoader.loadInput(5)));
+        System.out.println(getResult(InputLoader.loadInput(5).map(Seat::new)));
     }
 }
--- src/test/java/com/studio3104/adventofcode2020/day05/SeatTest.java
+++ src/test/java/com/studio3104/adventofcode2020/day05/SeatTest.java
@@ -7,20 +7,20 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-class Problem1Test {
+class SeatTest {
     static Stream<Arguments> argumentsProvider() {
         return Stream.of(
-                Arguments.of("FBFBBFFRLR", new int[]{44, 5}),
-                Arguments.of("BFFFBBFRRR", new int[]{70, 7}),
-                Arguments.of("FFFBBBFRRR", new int[]{14, 7}),
-                Arguments.of("BBFFBBFRLL", new int[]{102, 4})
+                Arguments.of("FBFBBFFRLR", new int[]{44, 5, 357}),
+                Arguments.of("BFFFBBFRRR", new int[]{70, 7, 567}),
+                Arguments.of("FFFBBBFRRR", new int[]{14, 7, 119}),
+                Arguments.of("BBFFBBFRLL", new int[]{102, 4, 820})
         );
     }
 
     @ParameterizedTest
     @MethodSource("argumentsProvider")
     void findPosition(String seat, int[] expected) {
-        int[] actual = Problem1.findPosition(seat);
-        Assertions.assertArrayEquals(expected, actual);
+        Seat s = new Seat(seat);
+        Assertions.assertArrayEquals(expected, new int[]{s.getRow(), s.getCol(), s.getId()});
     }
 }
