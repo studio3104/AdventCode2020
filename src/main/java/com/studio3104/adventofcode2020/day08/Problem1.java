5d6c2914e99908114e90159a361833a6fb0b47ff
studio3104 <studio3104.com@gmail.com>
2020-12-08 14:48:00 +0900

    Answer p1 of day 8

---
+++ src/main/java/com/studio3104/adventofcode2020/day08/Problem1.java
@@ -0,0 +1,48 @@
+package com.studio3104.adventofcode2020.day08;
+
+import com.studio3104.adventofcode2020.utilities.InputLoader;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Problem1 {
+    static int getResult(String[] stringCommands) {
+        Command[] commands = Arrays.stream(stringCommands)
+                .sequential()
+                .map(Command::new)
+                .toArray(Command[]::new);
+
+        int acc = 0;
+        int p = 0;
+        Set<Integer> done = new HashSet<>();
+
+        while (!done.contains(p)) {
+            done.add(p);
+
+            Command c = commands[p];
+            int n = c.getSign() == '+' ? c.getNum() : c.getNum() * -1;
+
+            switch (c.getOperation()) {
+                case "acc":
+                    ++p;
+                    acc += n;
+                    break;
+                case "jmp":
+                    p += n;
+                    break;
+                case "nop":
+                    ++p;
+                    break;
+                default:
+                    throw new RuntimeException();
+            }
+        }
+
+        return acc;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getResult(InputLoader.loadStringInput(8)));
+    }
+}
