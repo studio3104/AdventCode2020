195395b2a429f6ad9d882a7a843dc1d69b6cb3cf
studio3104 <studio3104.com@gmail.com>
2020-12-10 21:17:38 +0900

    Stop storing indexes into HashMap, implement an array instead

--- src/main/java/com/studio3104/adventofcode2020/day10/Problem2.java
+++ src/main/java/com/studio3104/adventofcode2020/day10/Problem2.java
@@ -2,30 +2,34 @@ package com.studio3104.adventofcode2020.day10;
 
 import com.studio3104.adventofcode2020.utilities.InputLoader;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Problem2 {
-    static long getResult(long[] adapters) {
+    static long getResult(long[] nums) {
+        Arrays.sort(nums);
+
         long device = Integer.MIN_VALUE;
-        Map<Long, Integer> indexOf = new HashMap<>();
+        long[] adapters = new long[nums.length + 2];
+
+        for (int i = 0; i < nums.length; ++i) {
+            adapters[i + 1] = nums[i];
+            device = Math.max(device, nums[i]);
+        }
+
+        adapters[adapters.length - 1] = device + 3;
+
+        long[] dp = new long[adapters.length];
 
         for (int i = 0; i < adapters.length; ++i) {
             long a = adapters[i];
-            device = Math.max(device, a);
-            indexOf.put(a, i + 1);
-        }
+            long b = a + 1, c = a + 2, d = a + 3;
 
-        device += 3;
-        indexOf.put((long) 0, 0);
-        indexOf.put(device, adapters.length + 1);
-
-        long[] dp = new long[adapters.length + 2];
-
-        for (long a : indexOf.keySet().stream().sorted().toArray(Long[]::new)) {
-            for (long n = a + 1; n <= a + 3; ++n) {
-                if (indexOf.containsKey(n)) {
-                    dp[indexOf.get(n)] += Math.max(1, dp[indexOf.get(a)]);
+            for (int j = i + 1; j <= i + 3 && j < adapters.length; ++j) {
+                long e = adapters[j];
+                if (e == b || e == c || e == d) {
+                    dp[j] += Math.max(1, dp[i]);
                 }
             }
         }
