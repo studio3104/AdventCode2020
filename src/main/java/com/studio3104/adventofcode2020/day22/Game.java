14f4e0701f4bdc223fa4f2b84ed9c46716a42cf2
studio3104 <studio3104.com@gmail.com>
2020-12-22 18:39:35 +0900

    Separate file

--- src/main/java/com/studio3104/adventofcode2020/day22/Game.java
+++ src/main/java/com/studio3104/adventofcode2020/day22/Game.java
@@ -1,41 +1,5 @@
 package com.studio3104.adventofcode2020.day22;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
-
-class Deck {
-    private final Deque<Integer> deck;
-
-    Deck(int[] cards) {
-        deck = new ArrayDeque<>();
-        Arrays.stream(cards).forEachOrdered(deck::add);
-    }
-
-    boolean isEmpty() {
-        return deck.isEmpty();
-    }
-
-    Integer draw() {
-        return deck.pollFirst();
-    }
-
-    void put(int card) {
-        deck.add(card);
-    }
-
-    int calculateScore() {
-        int score = 0;
-        int multipliedBy = 1;
-
-        while (!deck.isEmpty()) {
-            score += deck.pollLast() * multipliedBy++;
-        }
-
-        return score;
-    }
-}
-
 @lombok.RequiredArgsConstructor
 class Game {
     private final Deck player1;
