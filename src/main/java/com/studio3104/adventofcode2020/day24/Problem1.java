c58991a617f8f55371e2c543d85b80346795ca19
studio3104 <studio3104.com@gmail.com>
2020-12-25 06:33:07 +0900

    Update day 24 part 1

--- src/main/java/com/studio3104/adventofcode2020/day24/Problem1.java
+++ src/main/java/com/studio3104/adventofcode2020/day24/Problem1.java
@@ -4,69 +4,35 @@ import com.studio3104.adventofcode2020.utilities.InputLoader;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 class Problem1 {
-    private static void optimizeSteps(Map<String, Integer> steps, String d1, String d2, String t) {
-        int n = Math.min(steps.get(d1), steps.get(d2));
-        steps.put(t, steps.get(t) + n);
-        steps.put(d1, steps.get(d1) - n);
-        steps.put(d2, steps.get(d2) - n);
-    }
-
-    private static void optimizeSteps(Map<String, Integer> steps, String o1, String o2) {
-        int n = Math.min(steps.get(o1), steps.get(o2));
-        steps.put(o1, steps.get(o1) - n);
-        steps.put(o2, steps.get(o2) - n);
-    }
-
-    private static void optimizeSteps(Map<String, Integer> steps) {
-        optimizeSteps(steps, "nw", "sw", "w");
-        optimizeSteps(steps, "ne", "se", "e");
-        optimizeSteps(steps, "w", "ne", "nw");
-        optimizeSteps(steps, "w", "se", "sw");
-        optimizeSteps(steps, "e", "nw", "ne");
-        optimizeSteps(steps, "e", "sw", "se");
-
-        optimizeSteps(steps, "w", "e");
-        optimizeSteps(steps, "nw", "se");
-        optimizeSteps(steps, "sw", "ne");
-    }
-
-    private static void flipTile(Set<Map<String, Integer>> blackTiles, Map<String, Integer> steps) {
-        if (blackTiles.contains(steps)) {
-            blackTiles.remove(steps);
-        } else {
-            blackTiles.add(steps);
-        }
-    }
-
     static int getResult(String[] input) {
-        Set<Map<String, Integer>> blackTiles = new HashSet<>();
+        Set<List<Integer>> blackTiles = new HashSet<>();
 
-        for (String route : input) {
-            Map<String, Integer> steps = List.of("e", "w", "ne", "se", "nw", "sw")
-                    .stream()
-                    .collect(Collectors.toMap(s -> s, s -> 0));
+        for (String steps : input) {
+            int north = 0;
+            int east = 0;
 
-            for (int i = 0; i < route.length(); ++i) {
-                String d = String.valueOf(route.charAt(i));;
+            for (int i = 0; i < steps.length(); ++i) {
+                char d = steps.charAt(i);
 
-                if (d.equals("n") || d.equals("s")) {
-                    d += route.charAt(++i);
+                if (d == 'e' || d == 'w') {
+                    east = d == 'e' ? east + 2 : east - 2;
+                    continue;
                 }
 
-                steps.put(d, steps.get(d) + 1);
+                north = d == 'n' ? north + 1 : north - 1;
+                east = steps.charAt(++i) == 'e' ? east + 1 : east - 1;
             }
 
-            // Like `sync; sync; sync` haha...
-            optimizeSteps(steps);
-            optimizeSteps(steps);
-            optimizeSteps(steps);
+            List<Integer> tile = List.of(north, east);
 
-            flipTile(blackTiles, steps);
+            if (blackTiles.contains(tile)) {
+                blackTiles.remove(tile);
+            } else {
+                blackTiles.add(tile);
+            }
         }
 
         return blackTiles.size();
